62aac23caa6e2582a6c654d4ffe4f85363082a61
kkozlovsky <kkozlovsky@yandex.ru>
2024-02-25 20:06:39 +0300

    Added the checkstyle plugin

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM maven:3.9.6-eclipse-temurin-21-alpine AS build
 COPY /src /src
+COPY checkstyle-suppressions.xml /
 COPY pom.xml /
 RUN mvn -f /pom.xml clean package
 
--- src/main/java/ru/kerporation/datageneratormicroservice/DataGeneratorMicroserviceApplication.java
+++ src/main/java/ru/kerporation/datageneratormicroservice/DataGeneratorMicroserviceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DataGeneratorMicroserviceApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(DataGeneratorMicroserviceApplication.class, args);
     }
 
